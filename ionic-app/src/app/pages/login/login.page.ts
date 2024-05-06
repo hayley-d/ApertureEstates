50a40a1df03b8a6bd2939560867206d40d09b4cc
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-05-06 09:28:03 +0200

    Final Upload

--- ionic-app/src/app/pages/login/login.page.ts
+++ ionic-app/src/app/pages/login/login.page.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { AuthService } from '../../services/auth.service';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+  public email: string = "";
+  public password: string = "";
 
-  constructor() { }
+  constructor(private authService: AuthService, private toastController: ToastController,private router: Router) {}
 
   ngOnInit() {
+  }
+
+  async login() {
+    // Check if the password is 8 characters long
+    if (this.password.length < 8) {
+      this.presentToast('Password must be 8 characters long');
+      return;
+    }
+
+    // Check if the email is correct
+    if (!this.validateEmail(this.email)) {
+      this.presentToast('Invalid email');
+      return;
+    }
+
+    // Call the authentication service
+    const loginResult = await this.authService.loginUser(this.email, this.password).toPromise();
+
+    if (loginResult) {
+      this.presentToast('Login successful');
+      this.router.navigateByUrl('/listings');
+      // Redirect to another page or perform any additional actions for successful login
+    } else {
+      this.presentToast('Login failed');
+      // Handle unsuccessful login (e.g., display an error message)
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  // Function to validate email format
+  validateEmail(email: string): boolean {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
   }
 
 }
